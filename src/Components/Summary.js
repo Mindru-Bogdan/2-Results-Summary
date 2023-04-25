a38ee607def4b9bf72da2ad2996bca71a9e22487
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-04-25 19:31:45 +0300

    Desktop version

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -1,8 +1,38 @@
+import ReactionLogo from '../../src/icon-reaction.svg';
+import MemoryLogo from '../../src/icon-memory.svg';
+import VerbalLogo from '../../src/icon-verbal.svg';
+import VisualLogo from '../../src/icon-visual.svg';
+
 import classes from './Summary.module.css';
 
 const Summary = () => {
   return (
-    <div className={classes.card}>Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100 Continue</div>
+    <div className={classes.card}>
+      <h3 className={classes.title}>Summary</h3>
+      <ul>
+        <li className={classes.reaction}>
+          <img src={ReactionLogo}></img> Reaction
+          <ol className={classes.total}>/ 100</ol>
+          <ol className={classes.result}>80</ol>
+        </li>
+        <li className={classes.memory}>
+          <img src={MemoryLogo}></img> Memory
+          <ol className={classes.total}>/ 100</ol>
+          <ol className={classes.result}>92</ol>
+        </li>
+        <li className={classes.verbal}>
+          <img src={VerbalLogo}></img> Verbal
+          <ol className={classes.total}>/ 100</ol>
+          <ol className={classes.result}>61</ol>
+        </li>
+        <li className={classes.visual}>
+          <img src={VisualLogo}></img> Visual
+          <ol className={classes.total}>/ 100</ol>
+          <ol className={classes.result}>72</ol>
+        </li>
+      </ul>
+      <p className={classes.button}>Continue</p>
+    </div>
   );
 };
 
